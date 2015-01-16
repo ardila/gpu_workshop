32458f169fba6b9fa2b7b6ecad359f6d0bbb3b4c
Diego Ardila <ardila.d.ardila@gmail.com>
2015-01-16 08:55:38 -0800

    added code from tar

---
+++ grid/Ex1/example1.cpp
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// Code that reads values from a 2D grid and for each input point in the grid finds the minumum 
+// value among all values stored in a window around that element, and stores the minumum 
+// value in that output point.
+//cell = input, node = output
+// To compile it with GCC: g++ grid.cpp -lm
+// WSIZE determines size of window which affects amount of work
+#define WSIZE 16  
+#define DATAHSIZE 20000
+#define DATAWSIZE 14000
+#define CHECK_VAL 1
+#define MIN(X,Y) ((X<Y)?X:Y)
+int main()
+{
+    int i, j, sr, sc, tempout;
+    const int nr = DATAHSIZE; 
+    const int nc = DATAWSIZE;
+    const int wr = WSIZE;
+    const int wc = WSIZE;
+    typedef int oArray[DATAWSIZE];
+    typedef int iArray[DATAWSIZE+WSIZE];
+    oArray * output;
+    iArray * input;
+    if ((input = (iArray *)malloc(((nr+wr)*(nc+wc))*sizeof(int))) == 0) {printf("malloc fail\n"); exit(1);}
+    if ((output = (oArray *)malloc((nr*nc)*sizeof(int))) == 0) {printf("malloc fail\n"); exit(1); }
+    printf("Begin init\n");
+    memset(input, 0x7F, (nr+wr)*(nc+wc)*sizeof(int));
+    memset(output, 0x7F, (nr*nc)*sizeof(int));
+    for (i=0; i < nr+wr; i+=wr)
+      for (j=0; j < nc+wc; j+=wc)
+        input[i][j] = CHECK_VAL;
+    printf("Begin compute\n");
+    for(i=0; i<nr; i++)
+        for(j=0; j<nc; j++){
+          tempout = output[i][j];
+          for (sr=0; sr<wr; sr++)
+            for (sc=0; sc<wc; sc++)
+              if (tempout > input[i+sr][j+sc]) tempout = input[i+sr][j+sc];
+          output[i][j] = tempout;}
+    printf("Finished Compute\n");
+    for(i=0; i<nr; i++)
+        for(j=0; j<nc; j++)
+            if (output[i][j] != CHECK_VAL) {printf("mismatch at %d,%d, was: %d should be: %d\n", i,j,output[i][j], CHECK_VAL); return 1;}
+    printf("Results pass\n");
+    return 0;
+}
+
---
+++ grid/Ex3/outline3b.cu
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+// these are just for timing measurments
+#include <time.h>
+// Code that reads values from a 2D grid and for each node in the grid finds the minumum 
+// value among all values stored in cells sharing that node, and stores the minumum 
+// value in that node.
+
+// To compile it with nvcc execute: nvcc -O2 -o grid3 grid3.cu
+// Modified by Bob Crovella NVIDIA Corp. 12/2011 to demonstrate CUDA
+
+//define the window size (square window) and the data set size
+#define WSIZE 16 
+#define DATAHSIZE 20000 
+#define DATAWSIZE 14000
+#define CHECK_VAL 1
+#define MIN(X,Y) ((X<Y)?X:Y)
+#define BLKWSIZE 32
+#define BLKHSIZE 32
+
+#define cudaCheckErrors(msg) \
+    do { \
+        cudaError_t __err = cudaGetLastError(); \
+        if (__err != cudaSuccess) { \
+            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n", \
+                msg, cudaGetErrorString(__err), \
+                __FILE__, __LINE__); \
+            fprintf(stderr, "*** FAILED - ABORTING\n"); \
+            exit(1); \
+        } \
+    } while (0)
+
+typedef int oArray[DATAWSIZE];
+typedef int iArray[DATAWSIZE+WSIZE];
+
+__global__ void cmp_win(oArray *output, const iArray *input)
+{
+    __shared__ int smem[(BLKHSIZE + (WSIZE-1))][(BLKWSIZE + (WSIZE-1))];
+    int tempout, i, j;
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    int idy = blockIdx.y*blockDim.y + threadIdx.y;
+    if ((idx < DATAHSIZE+WSIZE) && (idy < DATAWSIZE+WSIZE))
+      smem[threadIdx.x][threadIdx.y]=input[idx][idy];
+    if ((idx < DATAHSIZE+WSIZE) && (idy < DATAWSIZE) && (threadIdx.y > BLKWSIZE - WSIZE))
+        smem[threadIdx.x][threadIdx.y + (WSIZE-1)] = input[idx][idy+(WSIZE-1)];
+    if ((idx < DATAHSIZE) && (idy < DATAWSIZE+WSIZE) && (threadIdx.x > BLKHSIZE - WSIZE))
+        smem[threadIdx.x + (WSIZE-1)][threadIdx.y] = input[idx+(WSIZE-1)][idy];
+    if ((idx < DATAHSIZE) && (idy < DATAWSIZE) && (threadIdx.x > BLKHSIZE - WSIZE) && (threadIdx.y > BLKWSIZE - WSIZE))
+        smem[threadIdx.x + (WSIZE-1)][threadIdx.y + (WSIZE-1)] = input[idx+(WSIZE-1)][idy+(WSIZE-1)];
+      
+    __syncthreads();
+    if ((idx < DATAHSIZE) && (idy < DATAWSIZE)){
+      tempout = output[idx][idy];
+      for (i=0; i<WSIZE; i++)
+        for (j=0; j<WSIZE; j++)
+// INSERT MINIMIZATION CODE HERE
+      output[idx][idy] = tempout;
+      }
+}
+
+int main(int argc, char *argv[])
+{
+    int i, j;
+    const dim3 blockSize(BLKHSIZE, BLKWSIZE, 1);
+    const dim3 gridSize(((DATAHSIZE+BLKHSIZE-1)/BLKHSIZE), ((DATAWSIZE+BLKWSIZE-1)/BLKWSIZE), 1);
+// these are just for timing
+    clock_t t0, t1, t2;
+    double t1sum=0.0;
+    double t2sum=0.0;
+// overall data set sizes
+    const int nr = DATAHSIZE;
+    const int nc = DATAWSIZE;
+// window dimensions
+    const int wr = WSIZE;
+    const int wc = WSIZE;
+// pointers for data set storage via malloc
+    iArray *h_in, *d_in;
+    oArray *h_out, *d_out;
+// start timing
+    t0 = clock();
+// allocate storage for data set
+    if ((h_in = (iArray *)malloc(((nr+wr)*(nc+wc))*sizeof(int))) == 0) {printf("malloc Fail \n"); exit(1);}
+    if ((h_out = (oArray *)malloc((nr*nc)*sizeof(int))) == 0) {printf("malloc Fail \n"); exit(1); }
+// synthesize data
+    printf("Begin init\n");
+    memset(h_in, 0x7F, (nr+wr)*(nc+wc)*sizeof(int));
+    memset(h_out, 0x7F, (nr*nc)*sizeof(int));
+    for (i=0; i<nr+wr; i+=wr)
+      for (j=0; j< nc+wc; j+=wc)
+        h_in[i][j] = CHECK_VAL;
+    t1 = clock();
+    t1sum = ((double)(t1-t0))/CLOCKS_PER_SEC;
+    printf("Init took %f seconds.  Begin compute\n", t1sum);
+// allocate GPU device buffers
+    cudaMalloc((void **) &d_in, (((nr+wr)*(nc+wc))*sizeof(int)));
+    cudaCheckErrors("Failed to allocate device buffer");
+    cudaMalloc((void **) &d_out, ((nr*nc)*sizeof(int)));
+    cudaCheckErrors("Failed to allocate device buffer2");
+// copy data to GPU
+    cudaMemcpy(d_out, h_out, ((nr*nc)*sizeof(int)), cudaMemcpyHostToDevice);
+    cudaCheckErrors("CUDA memcpy failure");
+    cudaMemcpy(d_in, h_in, (((nr+wr)*(nc+wc))*sizeof(int)), cudaMemcpyHostToDevice);
+    cudaCheckErrors("CUDA memcpy2 failure");
+
+    cmp_win<<<gridSize,blockSize>>>(d_out, d_in);
+    cudaCheckErrors("Kernel launch failure");
+// copy output data back to host
+
+    cudaMemcpy(h_out, d_out, ((nr*nc)*sizeof(int)), cudaMemcpyDeviceToHost);
+    cudaCheckErrors("CUDA memcpy3 failure");
+    t2 = clock();
+    t2sum = ((double)(t2-t1))/CLOCKS_PER_SEC;
+    printf ("Done. Compute took %f seconds\n", t2sum);
+    for (i=0; i < nr; i++)
+      for (j=0; j < nc; j++)
+        if (h_out[i][j] != CHECK_VAL) {printf("mismatch at %d,%d, was: %d should be: %d\n", i,j,h_out[i][j], CHECK_VAL); return 1;}
+    printf("Results pass\n");
+
+    return 0;
+}
+
